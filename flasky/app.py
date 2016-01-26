2ac0c490f018c3026093095795219aa33233c695
fengyingcai <fengyc.work@gmail.com>
2016-01-26 21:05:47 +0800

    update

--- flasky/app.py
+++ flasky/app.py
@@ -1,14 +1,35 @@
+import datetime
+import hashlib
+
 from flask import Flask, request, render_template
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = hashlib.sha256('TOP_SECRET'.encode()).hexdigest()
+
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     user_agent = request.headers.get('User-Agent')
-    return render_template('index.html', user_agent=user_agent)
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html',
+                           user_agent=user_agent,
+                           current_time=datetime.datetime.utcnow(),
+                           form=form,
+                           name=name)
 
 
 @app.route('/user/')
@@ -25,6 +46,12 @@ def handle_404():
 @app.errorhandler(500)
 def handle_500():
     return render_template('500.html')
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 
 if __name__ == '__main__':
