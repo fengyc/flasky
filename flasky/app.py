d3fcb1accb984b396e42fa9b54747aa835366215
fengyingcai <fengyc.work@gmail.com>
2016-01-27 00:57:26 +0800

    add db model

--- flasky/app.py
+++ flasky/app.py
@@ -1,5 +1,6 @@
 import datetime
 import hashlib
+import os
 
 from flask import Flask, request, render_template, session, redirect, url_for, flash
 from flask.ext.bootstrap import Bootstrap
@@ -7,14 +8,54 @@ from flask.ext.moment import Moment
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask.ext.sqlalchemy import SQLAlchemy
 
+basedir = os.path.abspath(os.path.pardir(__name__))
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = hashlib.sha256('TOP_SECRET'.encode()).hexdigest()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %s>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %s>' % self.username
+
+
+def init_db():
+    db.drop_all()
+    db.create_all()
+    admin_role = Role(name='Admin')
+    mod_role = Role(name='Moderator')
+    user_role = Role(name='User')
+    user_john = User(username='john', role=admin_role)
+    user_susan = User(username='susan', role=user_role)
+    user_david = User(username='david', role=user_role)
+    db.session.addall([admin_role, mod_role, user_role, user_john, user_susan,
+                       user_david])
+    db.session.commit()
+
 
 
 @app.route('/', methods=['GET', 'POST'])
